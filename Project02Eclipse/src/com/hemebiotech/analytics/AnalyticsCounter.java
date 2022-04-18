3984535228c307cbc2bf243bde9b25a68847e295
Marc MASSE <marc.masse600@orange.fr>
2022-04-18 16:23:19 +0200

    Unused line removed

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -14,7 +14,6 @@ public class AnalyticsCounter {
 		BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
 
-		int headCount = 0; // counts headaches
 		while (line != null) {
 			if (line.equals("headache")) {
 				headacheCount++;
