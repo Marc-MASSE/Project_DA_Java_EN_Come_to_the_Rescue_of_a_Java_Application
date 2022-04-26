acda7d7f1d8d60ed64d519382d1fba433673f9c2
Marc MASSE <marc.masse600@orange.fr>
2022-04-26 18:06:21 +0200

    OccurenceCounterList : foreach loop instead of for loop

--- Project02Eclipse/src/com/hemebiotech/analytics/OccurenceCounterList.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/OccurenceCounterList.java
@@ -1,5 +1,6 @@
 package com.hemebiotech.analytics;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.TreeMap;
 
@@ -25,37 +26,15 @@ public class OccurenceCounterList implements IOccurenceCounter {
 	@Override
 	public TreeMap<String, Integer> getOccurences() {
 
-		/*
-		 * Initialisation de la TreeMap Son premier élément a pour clé le premier
-		 * élément de list et pour valeur 1
-		 */
-
 		TreeMap<String, Integer> treeMap = new TreeMap<>();
-		treeMap.put(list.get(0), 1);
 
-		for (int i = 1; i < list.size(); i++) {
-
-			String key;
-			int value;
-
-			key = list.get(i);
-
-			/*
-			 * Si l'élément suivant de list est identique au précédent, l'élément TreeMap de
-			 * clé key voit sa valeur augmenter de 1
-			 */
-			if (list.get(i).equals(list.get(i - 1))) {
-				value = treeMap.get(key);
-				treeMap.replace(key, value + 1);
+		for (String key : list) {
+			if (!treeMap.containsKey(key)) {
+				treeMap.put(key, Collections.frequency(list, key));
 			}
-			/*
-			 * Sinon, un nouvel élément est ajouté à la TreeMap avec comme clé key et comme
-			 * valeur 1
-			 */
-			else {
-				treeMap.put(key, 1);
-			}
+
 		}
+
 		return treeMap;
 	}
 
